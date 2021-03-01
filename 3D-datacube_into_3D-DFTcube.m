3b2813bf92b73ff06acac88f2042a6db3204a6cf
giocic2 <74670040+giocic2@users.noreply.github.com>
2021-03-01 18:32:21 +0100

    New code using 3D FFT.

---
+++ 3D-datacube_into_3D-DFTcube.m
@@ -0,0 +1,17 @@
+% data la matrice del datacube il codice dovrà eseguire DTFT lungo le tre
+% direzioni della matrice
+clear
+clc
+
+fastTimeIndex = 100
+slowTimeIndex = 10
+spatialIndex = 2
+
+% Number of bins for the FFT computation, for each dimension. Should be a power of 2.
+bins_fastTime = 2^nextpow2(1000)
+bins_slowTime = 2^nextpow2(1000)
+bins_spatial = 2^nextpow2(1000)
+
+datacube = zeros(fastTimeIndex,slowTimeIndex,spatialIndex);
+
+DFTcube = fftn(datacube,[bins_fastTime bins_slowTime bins_spatial]);
