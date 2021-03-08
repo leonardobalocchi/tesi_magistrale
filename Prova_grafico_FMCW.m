af759aab23eef087fa4180c0850a3b0813e0c815
giocic2 <74670040+giocic2@users.noreply.github.com>
2021-03-08 14:44:02 +0100

    Update.

---
+++ Prova_grafico_FMCW.m
@@ -0,0 +1,33 @@
+% originale
+
+fs = 500e3;
+sLFM = phased.LinearFMWaveform('SampleRate',fs,...
+    'SweepBandwidth',200e3,...
+    'PulseWidth',1e-3,'PRF',1e3);
+
+lfmwav = step(sLFM);
+nsamp = size(lfmwav,1);
+t = [0:(nsamp-1)]/fs;
+plot(t*1000,real(lfmwav))
+xlabel('Time (millisec)')
+ylabel('Amplitude')
+grid
+
+% Waveform Specs
+fs = 4e3;
+sweep_bandwidth = 670e6;
+sweep_slope = 21e12; %supponendo che in matlab lo slope sia in secondi e non in microsecondi, quindi devo convertire in secondi
+pulse_width = sweep_bandwidth/sweep_slope;
+prf = 250;
+sLFM = phased.LinearFMWaveform('SampleRate',fs,...
+    'SweepBandwidth',sweep_bandwidth,...
+    'PulseWidth',pulse_width,'PRF',prf);
+
+lfmwav = step(sLFM);
+nsamp = size(lfmwav,1);
+t = [0:(nsamp-1)]/fs;
+plot(t*1000,real(lfmwav))
+xlabel('Time (millisec)')
+ylabel('Amplitude')
+grid
+
